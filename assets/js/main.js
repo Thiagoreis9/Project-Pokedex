6f019c9dc62a86e81ed3f491cad378f9da3da846
Thiago_Reis <reist36@gmail.com>
2022-12-06 12:16:44 -0300

    Modifying pokedex layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,29 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 const pokemonDetail = document.getElementById('pokemonDetail')
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 1100
+const limit = 50
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    return `<a href="../poke-detail.html">
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-        </a>
-    `
+    return `<a href="poke-detail.html">
+                <li class="pokemon ${pokemon.type}">
+                    <div class="pokemonLayout">
+                        <span class="number ${pokemon.type} numberBG">#${pokemon.number}</span>
+                        
+                        <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+                            <img src="${pokemon.photo}"
+                            alt="${pokemon.name}">
+                        </div>
+                    </div>
+                    <div>
+                        <span class="name">${pokemon.name}</span>
+                    </div>
+                </li>
+            </a>`
 }
 
 function loadPokemonItens(offset, limit) {
