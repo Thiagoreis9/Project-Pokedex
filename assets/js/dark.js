919c822d28c1265cc9984e9a4ddf119228c63327
Thiago_Reis <reist36@gmail.com>
2022-12-06 21:01:39 -0300

    Creating darkmode delay transition

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -2,26 +2,31 @@ let body = document.querySelector('body')
 
 
 function darkMode(){
-
     
     document.getElementById("body").style.backgroundImage = "url(../assets/imagens/background1.jpg)";
+    document.getElementById("body").style.transitionDuration = '2000ms';
 
-    //document.getElementsByClassName(".pokemonLayout").style.backgroundColor = "black";
     const pokemonsBlack = document.getElementsByClassName("pokemonLayout");
     for (let i = 0; i < pokemonsBlack.length; i++) {
         pokemonsBlack[i].style.backgroundColor = "rgba(0,0,0,0.5)";
+        
     }
-
     document.getElementById("content").style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    document.getElementById("content").style.transitionDuration = '2500ms';
 
 }
+
 function lightMode(){
 
     document.getElementById("body").style.backgroundImage = "url(../assets/imagens/background.jpg)";
+    document.getElementById("body").style.transitionDuration = '2000ms'
 
     const pokemonsWhite = document.getElementsByClassName("pokemonLayout");
     for (let i = 0; i < pokemonsWhite.length; i++) {
         pokemonsWhite[i].style.backgroundColor = "#fff";
+        pokemonsWhite[i].style.transitionDuration = '2500ms';
     }
     document.getElementById("content").style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+    document.getElementById("content").style.transitionDuration = '2500ms';
+
 }
